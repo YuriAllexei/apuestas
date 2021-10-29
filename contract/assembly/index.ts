c7dccdcd23a3fdb3e757297b19261bf71f919f36
yuriallexei <yurialexei@ciencias.unam.mx>
2021-10-29 01:28:45 -0500

    Problema build

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -18,7 +18,7 @@ import {Apuesta,apuestasRegistry,jugadorRegistry,Jugador,Deuda, Solicitud} from 
 export function setState(nombre:string,nearID:string,prestamo:boolean): string{
 
   if (jugadorRegistry.contains(nearID)) {
-    return `Ya hay una cuenta asociada a: ${nearID}`
+    return `Ya hay una cuenta asociada a: ${nearID}    ${jugadorRegistry}`
   }
   else{
     jugadorRegistry.set(nearID,new Jugador(nombre,nearID,prestamo))
@@ -27,19 +27,24 @@ export function setState(nombre:string,nearID:string,prestamo:boolean): string{
   }
 }
 
-export function solicitarPrestamo(idPrestamista:string,idPrestatario:string,cantidad:number): string {
+export function solicitarPrestamo(idPrestamista:string,idPrestatario:string,cantidad:f64): string {
   if (jugadorRegistry.contains(idPrestamista) && jugadorRegistry.contains(idPrestatario) && idPrestatario!=idPrestamista) {
     const prestamista= jugadorRegistry.get(idPrestamista)
     const prestatario= jugadorRegistry.get(idPrestatario)
+    
+    if (prestamista==null || prestatario==null) {
+      return `${prestamista?.toString()}       ${prestatario?.toString()}`
+    }
 
-    prestamista!.requests.set(idPrestatario,new Solicitud("Adeudo",prestatario!,cantidad,0.07))
-    prestatario!.requests.set(idPrestamista,new Solicitud("Deuda",prestamista!,cantidad,0.07))
+    prestamista.requests.set(idPrestatario,new Solicitud("Adeudo",prestatario,cantidad,0.07))
+    prestatario.requests.set(idPrestamista,new Solicitud("Deuda",prestamista,cantidad,0.07))
 
-    jugadorRegistry.set(idPrestamista,prestamista!)
-    jugadorRegistry.set(idPrestatario,prestatario!)
+    jugadorRegistry.set(idPrestamista,prestamista)
+    jugadorRegistry.set(idPrestatario,prestatario)
 
     return "Solicitud de préstamo enviada"
   }
+  
   else{
     return "Solicitud de prestamo fracasada. Revise los ID's de near de nuevo."
   }
@@ -52,7 +57,7 @@ export function solicitarPrestamo(idPrestamista:string,idPrestatario:string,cant
 
 //   if (jugadorRegistry.contains(nearID)) {
 //     const manejador=jugadorRegistry.get(nearID)
-//     manejador!.requests.forEach((value: Solicitud, key: string) => {
+//     manejador.requests.forEach((value: Solicitud, key: string) => {
 //       const solicitante=value.jugador
 //       const cantidad=value.cantidad
 //       const tipo=value.tipo
@@ -70,26 +75,33 @@ export function solicitarPrestamo(idPrestamista:string,idPrestatario:string,cant
 
 
 
-export  function aceptarSolicitudes(idAjena:string,aceptar:boolean){
+export  function aceptarSolicitudes(idAjena:string,aceptar:boolean) : string{
   const idPropia=Context.sender
   const decide=jugadorRegistry.get(idPropia)
+
   const depende=jugadorRegistry.get(idPropia)
-  const solicitud=decide!.requests.get(idAjena)
+  if (decide==null || depende==null) {
+    return "Jugador no encontrado"
+  }
+  const solicitud=decide.requests.get(idAjena)
+  if (solicitud==null) {
+    return "No tienes solicitudes de ningun tipo."
+  }
   if (aceptar) {
-    if (solicitud!.tipo=="Adeudo") {
-      const deuda=solicitud!.cantidad*(1+solicitud!.interes)
+    if (solicitud.tipo=="Adeudo") {
+      const deuda=solicitud.cantidad*(1+solicitud.interes)
       //Deposito de near nivel 2
       
-      decide!.adeudos.push(new Deuda(depende!,deuda,2021,7,27,"Adeudo"))
-      decide!.fondos-=deuda
-      decide!.transacciones.set("OtorgoPrestamo",deuda)
-      depende!.deudores.push(new Deuda(decide!,deuda,2021,7,27,"Deuda"))
-      depende!.fondos+=deuda
-      depende!.transacciones.set("RecibioPrestamo",deuda)
-      decide!.requests.delete(idAjena)
-      depende!.requests.delete(idPropia)
-      jugadorRegistry.set(idPropia,decide!)
-      jugadorRegistry.set(depende!.idNear,depende!)
+      decide.adeudos.push(new Deuda(depende,deuda,"Adeudo"))
+      decide.fondos-=deuda
+      decide.transacciones.set("OtorgoPrestamo",deuda)
+      depende.deudores.push(new Deuda(decide,deuda,"Deuda"))
+      depende.fondos+=deuda
+      depende.transacciones.set("RecibioPrestamo",deuda)
+      decide.requests.delete(idAjena)
+      depende.requests.delete(idPropia)
+      jugadorRegistry.set(idPropia,decide)
+      jugadorRegistry.set(depende.idNear,depende)
 
       return "Se ha aceptado la deuda. Revisa tu cartera."
     } else {
@@ -97,42 +109,37 @@ export  function aceptarSolicitudes(idAjena:string,aceptar:boolean){
     }
   }
   else{
-      decide!.requests.delete(idAjena)
-      depende!.requests.delete(idPropia)
-      jugadorRegistry.set(idPropia,decide!)
-      jugadorRegistry.set(depende!.idNear,depende!)
+      decide.requests.delete(idAjena)
+      depende.requests.delete(idPropia)
+      jugadorRegistry.set(idPropia,decide)
+      jugadorRegistry.set(depende.idNear,depende)
     return "El jugador no ha aceptado tu solicitud de prestamo"
   }
 }
 
-export function setApuesta(entrada:number,idNear:string,tipo:string,resultado:string) : string{
+export function setApuesta(entrada:f64,idNear:string,tipo:string,resultado:string) : string{
   if (jugadorRegistry.contains(idNear)) {
     const actual=jugadorRegistry.get(idNear)
-    actual!.fondos-=entrada
+    if (actual==null) {
+      return "Jugador no encontrado"
+    }
+    actual.fondos-=entrada
     const ap=new Apuesta(entrada,idNear,tipo,resultado)
-    actual!.historial.push(ap)
+    actual.historial.push(ap)
     return `Apuesta registrada exitosamente ${ap.apuestaToString()}`
   }
 else{
   return `El jugador no fue encontrado`
-}
+  }
 }
 
 
-export function getJugador(idNear:string) {
-  if (jugadorRegistry.contains(idNear)) {
-    logging.log(jugadorRegistry.get(idNear)?.toString())
-  }
-  else{
-    logging.log("El jugador no existe")
-  }
-}
 
 
 const DEFAULT_MESSAGE = 'Hello'
 
 // Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
+// Feel free to extract behavior to non-exported functions
 export function getGreeting(accountId: string): string | null {
   // This uses raw `storage.get`, a low-level way to interact with on-chain
   // storage for simple contracts.
@@ -144,7 +151,7 @@ export function getGreeting(accountId: string): string | null {
 export function setGreeting(message: string): void {
   const account_id = Context.sender
 
-  // Use logging.log to record logs permanently to the blockchain!
+  // Use logging.log to record logs permanently to the blockchain
   logging.log(
     // String interpolation (`like ${this}`) is a work in progress:
     // https://github.com/AssemblyScript/assemblyscript/pull/1115
--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -5,22 +5,19 @@ export class Deuda{
 
    tipo: string
    jugador: Jugador;
-   cantidad: number;
-   fechaCreado: Date;
-   fechaSaldado: Date; 
-  saldado:boolean
+   cantidad: f64;
+    saldado:boolean
 
-  constructor(jugador: Jugador,cantidad:number,año:number,mes:number,dia:number,tipo:string){
+  constructor(jugador: Jugador,cantidad:f64,tipo:string){
     this.jugador=jugador
     this.cantidad=cantidad
-    this.fechaCreado=new Date(500000)
     this.saldado=false
     this.tipo=tipo
   }
 
   imprimirDeuda():string{
-    const saldado :string =this.saldado ? this.fechaSaldado.toDateString() : "No ha sido saldada"
-    return `{Persona:${this.jugador.toDeuda(this.tipo)},Cantidad:${this.cantidad},FechaDeuda:${this.fechaCreado.toDateString()},FechaSaldado:${saldado}}` 
+ 
+    return `{Persona:${this.jugador.toDeuda(this.tipo)},Cantidad:${this.cantidad}`
     
     
   }
@@ -31,11 +28,11 @@ export class Deuda{
   export class Solicitud{
     tipo: string
     jugador: Jugador
-    cantidad: number
-    interes: number
+    cantidad: f64
+    interes: f64
 
 
-    constructor(tipo:string,jugador: Jugador,cantidad:number,interes:number){
+    constructor(tipo:string,jugador: Jugador,cantidad:f64,interes:f64){
       this.tipo=tipo
       this.jugador=jugador
       this.cantidad=cantidad
@@ -53,26 +50,26 @@ export class Deuda{
   export class Jugador{
      nombreJ: string;
      idNear: string;
-     fondos: number;
-     transacciones: PersistentUnorderedMap<string,number>;
+     fondos: f64;
+     transacciones: PersistentUnorderedMap<string,f64>;
      adeudos: PersistentVector<Deuda>;
      deudores: PersistentVector<Deuda>;
      historial: PersistentVector<Apuesta>;
      verificado: boolean;
-     deuda: number;
-     adeudo: number;
+     deuda: f64;
+     adeudo: f64;
      prestamos: boolean;
-    requests: Map<string,Solicitud>;
+     requests: PersistentUnorderedMap<string,Solicitud>;
     constructor(nombreJ:string,idNear:string,prestamo:boolean){
         this.nombreJ=nombreJ
         this.idNear=idNear
         this.adeudos=new PersistentVector<Deuda>(idNear+"a")
         this.deudores=new PersistentVector<Deuda>(idNear+"d")
         this.historial=new PersistentVector<Apuesta>("h")
-        this.transacciones=new PersistentUnorderedMap<string,number>("t")
+        this.transacciones=new PersistentUnorderedMap<string,f64>("t")
         this.prestamos=prestamo
         this.verificado=false
-        this.requests=new Map<string,Solicitud>()
+        this.requests=new PersistentUnorderedMap<string,Solicitud>("r")
     }
 
     toDeuda(tipo:string):string{
@@ -92,21 +89,20 @@ export class Deuda{
     }   
 }
 export class Apuesta{
-    entrada: number
-    jugadores: PersistentVector<Jugador> | null
-    tipo: number
+    entrada: f64
+    jugador: string
+    tipo: string
     resultado: string
-    len: number  
+    
 
-  constructor(entrada: number,jugadores: PersistentVector<Jugador> | null,tipo: number,resultado:string){
+  constructor(entrada: f64,jugador:string ,tipo: string,resultado:string){
     this.entrada=entrada
-    this.jugadores=jugadores
+    this.jugador=jugador
     this.tipo=tipo
     this.resultado=resultado
-    this.len =jugadores ? jugadores.length : 0
   }
   apuestaToString(): string{
-    return `{entrada:${this.entrada},jugadores:${this.jugadores},tipo:${this.tipo},resultado:${this.resultado},len:${this.len}}`
+    return `{entrada:${this.entrada},jugadores:${this.jugador},tipo:${this.tipo},resultado:${this.resultado}}`
   }
 }
 
